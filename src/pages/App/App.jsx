81152a9e137cf35ca1c4df17d3890d07c80adfc4
David Anderson <djathree92@gmail.com>
2021-06-06 14:26:55 -0400

    fix auth

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -31,7 +31,7 @@ class App extends Component {
           path="/"
           render={() => (
             <main>
-              <h1>Welcome. This is an authorization template.</h1>
+              <h1>Welcome, Sneakerhead.</h1>
             </main>
           )}
         />
